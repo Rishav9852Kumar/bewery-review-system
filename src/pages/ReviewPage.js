55621bcda88a5bcbaee17d80fbad078bac4bb65a
Rishav Kumar <rishav@whydonate.com>
2023-11-10 01:10:53 +0530

    home pge initiated

--- src/pages/ReviewPage.js
+++ src/pages/ReviewPage.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext} from "react";
 import "../utils/PageNotFound.css";
+import { ReviewContext } from "../context/reviewContext";
 
 const ReviewPage = () => {
+  const reviewContext = useContext(ReviewContext);
   return (
     <div className="not-found-container">
       <h1>This is the review page </h1>
+      <h2>Review Id: {reviewContext.reviewId}</h2>
     </div>
   );
 };
